1241127eda1530a8b03d23a42e2405422df7cf23
abhay313 <abhayiiitk@gmail.com>
2020-12-30 14:52:30 +0530

    added humidity, wind speed and description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,10 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find the location, try another search', undefined)
         }else{
-            callback(undefined, 'Current temperature ' + body.current.temperature + ' but feels like ' + body.current.feelslike + '. It is ' +  body.current.weather_descriptions[0])
+            callback(
+                undefined, 
+                'Current temperature ' + body.current.temperature + ' but feels like ' + body.current.feelslike + '. It is ' +  body.current.weather_descriptions[0] + 
+                '. Also humidity is ' + body.current.humidity + '% and wind speed is about ' + body.current.wind_speed + 'mph')
         }
 
     })
